a580d68d2d29713271a59849da3d20425ff88ea6
Евгений Глущенко <dartmol21@mail.ru>
2023-01-19 23:55:40 +0300

    add js for client form

--- static/js/dropdown.js
+++ static/js/dropdown.js
@@ -18,3 +18,37 @@ window.onclick = function(event) {
     }
   }
 }
+
+function for_checkbox_phys(){
+
+  let div = document.getElementById('physical-person-block');
+  let entity = document.getElementById('entity');
+  let entity_div = document.getElementById('entity-block');
+
+  if(this.checked){
+      div.style.display = 'block';
+      entity.checked = false;
+      entity_div.style.display = 'none'
+  }
+  else
+    div.style.display = 'none';
+}
+
+
+function for_checkbox_entity(){
+
+  let div = document.getElementById('entity-block');
+  let phys = document.getElementById('physical-person');
+  let phys_div = document.getElementById('physical-person-block');
+
+  if(this.checked){
+    div.style.display = 'block';
+    phys.checked = false;
+    phys_div.style.display = 'none'
+  }
+  else
+    div.style.display = 'none';
+}
+
+document.getElementById('physical-person').onchange = for_checkbox_phys
+document.getElementById('entity').onchange = for_checkbox_entity
