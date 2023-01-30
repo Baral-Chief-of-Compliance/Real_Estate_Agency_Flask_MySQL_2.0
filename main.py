cb0f08f00b32ea4d0393132f576777054ff1c0d6
Евгений Глущенко <dartmol21@mail.ru>
2023-01-30 10:27:34 +0300

    add env data and css for adaptation

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
 from flask import Flask, request, render_template, redirect, url_for,  session
 from flask_mysqldb import MySQL
+from dotenv import load_dotenv
+import os
 import MySQLdb.cursors
 from config import Config
+
+load_dotenv()
 
 
 app = Flask(__name__)
 app.config.from_object(Config)
 
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = '1234'
-app.config['MYSQL_DB'] = 'real_estate_agency_3'
+app.config['MYSQL_HOST'] = os.getenv('MYSQL_HOST')
+app.config['MYSQL_USER'] = os.getenv('MYSQL_USER')
+app.config['MYSQL_PASSWORD'] = os.getenv('MYSQL_PASSWORD')
+app.config['MYSQL_DB'] = os.getenv('MYSQL_DB')
 
 mysql = MySQL(app)
 
