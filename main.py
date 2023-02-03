2774c76df62bc85edd67d7576c811fc80379c38b
Евгений Глущенко <dartmol21@mail.ru>
2023-02-03 10:17:48 +0300

    refactoring code config

--- main.py
+++ main.py
@@ -10,12 +10,6 @@ load_dotenv()
 
 app = Flask(__name__)
 app.config.from_object(Config)
-
-app.config['MYSQL_HOST'] = os.getenv('MYSQL_HOST')
-app.config['MYSQL_USER'] = os.getenv('MYSQL_USER')
-app.config['MYSQL_PASSWORD'] = os.getenv('MYSQL_PASSWORD')
-app.config['MYSQL_DB'] = os.getenv('MYSQL_DB')
-
 mysql = MySQL(app)
 
 
